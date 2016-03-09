6856ba429a5edf508bf990fa9c2124e30aab6883
Victor Chabbert <victor.chabbert@eisti.fr>
2016-03-09 10:19:52 +0100

    Added improved devtools inspector from alexkuz

--- src/containers/DevTools.js
+++ src/containers/DevTools.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 import { createDevTools } from 'redux-devtools'
 
-import LogMonitor from 'redux-devtools-log-monitor'
+import Inspector from 'redux-devtools-inspector'
 import DockMonitor from 'redux-devtools-dock-monitor'
 
 const DevTools = createDevTools(
   <DockMonitor toggleVisibilityKey="ctrl-h"
                changePositionKey="ctrl-q">
-    <LogMonitor />
+    <Inspector theme="monokai"/>
   </DockMonitor>
 )
 
